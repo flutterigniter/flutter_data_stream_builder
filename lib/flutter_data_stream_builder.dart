618b273df1c1e0c85894dc541b5223ced1c1ba0d
Frank Treacy <me@frank06.net>
2019-12-05 19:17:09 -0300

    formatting

--- lib/flutter_data_stream_builder.dart
+++ lib/flutter_data_stream_builder.dart
@@ -3,17 +3,18 @@ library flutter_data_stream_builder;
 import 'package:flutter/material.dart';
 
 typedef DataWidgetBuilder<T> = Widget Function(BuildContext context, T data);
-typedef DataErrorWidgetBuilder = Widget Function(BuildContext context, dynamic error);
+typedef DataErrorWidgetBuilder = Widget Function(
+    BuildContext context, dynamic error);
 
 /// Widget that builds itself based on the latest
 /// loading, error and data states from a [Stream].
-/// 
+///
 /// {@tool sample}
 ///
 /// This sample shows a [DataStreamBuilder] rendering a list of posts
 /// provided by an underlying websocket connection.
 ///
-/// 
+///
 /// ```dart
 /// DataStreamBuilder<List<Post>>(
 ///   stream: Repository.of<Post>().findAll(),
@@ -22,9 +23,9 @@ typedef DataErrorWidgetBuilder = Widget Function(BuildContext context, dynamic e
 ///   )
 /// )
 /// ```
-/// 
+///
 /// Loading and error state rendering can be supplied as follows:
-/// 
+///
 /// ```dart
 /// DataStreamBuilder<List<Post>>(
 ///   stream: Repository.of<Post>().findAll(),
@@ -35,9 +36,9 @@ typedef DataErrorWidgetBuilder = Widget Function(BuildContext context, dynamic e
 ///   )
 /// )
 /// ```
-/// 
+///
 /// {@end-tool}
-/// 
+///
 /// See also:
 ///
 ///  * [StreamBuilder]
@@ -46,44 +47,47 @@ class DataStreamBuilder<T> extends StreamBuilderBase<T, AsyncSnapshot<T>> {
   /// loading, error and data states from the specified [stream] and whose build
   /// strategies are given by [loadingBuilder], [errorBuilder] and [builder],
   /// respectively.
-  /// 
+  ///
   /// Sensible defaults are provided for [loadingBuilder] and [errorBuilder].
   ///
   /// If you wish to supply initial data, override [loadingBuilder].
   ///
   /// The [stream] and [builder] must not be null.
-  const DataStreamBuilder({
-    Key key,
-    @required Stream<T> stream,
-    @required this.builder,
-    WidgetBuilder loadingBuilder,
-    DataErrorWidgetBuilder errorBuilder
-  }) :
-    assert(builder != null),
-    this.loadingBuilder = loadingBuilder,
-    this.errorBuilder = errorBuilder,
-    super(key: key, stream: stream);
+  const DataStreamBuilder(
+      {Key key,
+      @required Stream<T> stream,
+      @required this.builder,
+      WidgetBuilder loadingBuilder,
+      DataErrorWidgetBuilder errorBuilder})
+      : assert(builder != null),
+        this.loadingBuilder = loadingBuilder,
+        this.errorBuilder = errorBuilder,
+        super(key: key, stream: stream);
 
   /// The build strategy used by this builder to render the loading state.
   final WidgetBuilder loadingBuilder;
 
   /// The build strategy used by this builder to render the error state.
   final DataErrorWidgetBuilder errorBuilder;
-  
+
   /// The build strategy used by this builder to render data.
   final DataWidgetBuilder<T> builder;
 
   @override
-  AsyncSnapshot<T> initial() => AsyncSnapshot<T>.withData(ConnectionState.none, null);
+  AsyncSnapshot<T> initial() =>
+      AsyncSnapshot<T>.withData(ConnectionState.none, null);
 
   @override
-  AsyncSnapshot<T> afterConnected(_) => AsyncSnapshot<T>.withData(ConnectionState.waiting, null);
+  AsyncSnapshot<T> afterConnected(_) =>
+      AsyncSnapshot<T>.withData(ConnectionState.waiting, null);
 
   @override
-  AsyncSnapshot<T> afterData(_, T data) => AsyncSnapshot<T>.withData(ConnectionState.active, data);
+  AsyncSnapshot<T> afterData(_, T data) =>
+      AsyncSnapshot<T>.withData(ConnectionState.active, data);
 
   @override
-  AsyncSnapshot<T> afterError(_, Object error) => AsyncSnapshot<T>.withError(ConnectionState.active, error);
+  AsyncSnapshot<T> afterError(_, Object error) =>
+      AsyncSnapshot<T>.withError(ConnectionState.active, error);
 
   @override
   AsyncSnapshot<T> afterDone(AsyncSnapshot<T> current) => current;
@@ -99,20 +103,21 @@ class DataStreamBuilder<T> extends StreamBuilderBase<T, AsyncSnapshot<T>> {
   }
 
   Widget _getLoading(BuildContext context) {
-    return loadingBuilder?.call(context) ?? Center(child: CircularProgressIndicator());
+    return loadingBuilder?.call(context) ??
+        Center(child: CircularProgressIndicator());
   }
 
   Widget _getError(BuildContext context, dynamic error) {
-    final _errorBuilder = errorBuilder ?? (context, dynamic error) {
-      error = error is Exception ? error.toString() : 'Error: $error';
-      return Center(child: Text(
-          error,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(backgroundColor: Colors.red, color: Colors.white),
-        )
-      );
-    };
+    final _errorBuilder = errorBuilder ??
+        (context, dynamic error) {
+          error = error is Exception ? error.toString() : 'Error: $error';
+          return Center(
+              child: Text(
+            error,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(backgroundColor: Colors.red, color: Colors.white),
+          ));
+        };
     return _errorBuilder.call(context, error);
   }
-
 }
