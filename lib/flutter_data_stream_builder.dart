08abf56fc3ec4607ae93930e0651677146d16bd1
Frank Treacy <me@frank06.net>
2019-12-05 15:49:16 -0300

    first commit

---
+++ lib/flutter_data_stream_builder.dart
@@ -0,0 +1,118 @@
+library flutter_data_stream_builder;
+
+import 'package:flutter/material.dart';
+
+typedef DataWidgetBuilder<T> = Widget Function(BuildContext context, T data);
+typedef DataErrorWidgetBuilder = Widget Function(BuildContext context, dynamic error);
+
+/// Widget that builds itself based on the latest
+/// loading, error and data states from a [Stream].
+/// 
+/// {@tool sample}
+///
+/// This sample shows a [DataStreamBuilder] rendering a list of posts
+/// provided by an underlying websocket connection.
+///
+/// 
+/// ```dart
+/// DataStreamBuilder<List<Post>>(
+///   stream: Repository.of<Post>().findAll(),
+///   builder: (context, List<Post> posts) => ListView(
+///     children: posts.map((post) => ListTile(title: Text(post.body))).toList(),
+///   )
+/// )
+/// ```
+/// 
+/// Loading and error state rendering can be supplied as follows:
+/// 
+/// ```dart
+/// DataStreamBuilder<List<Post>>(
+///   stream: Repository.of<Post>().findAll(),
+///   loadingBuilder: (context) => Center(child: Text('Loading posts...')),
+///   errorBuilder: (context, error) => PostErrorView(error),
+///   builder: (context, List<Post> posts) => ListView(
+///     children: posts.map((post) => ListTile(title: Text(post.body))).toList(),
+///   )
+/// )
+/// ```
+/// 
+/// {@end-tool}
+/// 
+/// See also:
+///
+///  * [StreamBuilder]
+class DataStreamBuilder<T> extends StreamBuilderBase<T, AsyncSnapshot<T>> {
+  /// Creates a new [DataStreamBuilder] that builds itself based on the latest
+  /// loading, error and data states from the specified [stream] and whose build
+  /// strategies are given by [loadingBuilder], [errorBuilder] and [builder],
+  /// respectively.
+  /// 
+  /// Sensible defaults are provided for [loadingBuilder] and [errorBuilder].
+  ///
+  /// If you wish to supply initial data, override [loadingBuilder].
+  ///
+  /// The [stream] and [builder] must not be null.
+  const DataStreamBuilder({
+    Key key,
+    @required Stream<T> stream,
+    @required this.builder,
+    WidgetBuilder loadingBuilder,
+    DataErrorWidgetBuilder errorBuilder
+  }) :
+    assert(builder != null),
+    this.loadingBuilder = loadingBuilder,
+    this.errorBuilder = errorBuilder,
+    super(key: key, stream: stream);
+
+  /// The build strategy used by this builder to render the loading state.
+  final WidgetBuilder loadingBuilder;
+
+  /// The build strategy used by this builder to render the error state.
+  final DataErrorWidgetBuilder errorBuilder;
+  
+  /// The build strategy used by this builder to render data.
+  final DataWidgetBuilder<T> builder;
+
+  @override
+  AsyncSnapshot<T> initial() => AsyncSnapshot<T>.withData(ConnectionState.none, null);
+
+  @override
+  AsyncSnapshot<T> afterConnected(_) => AsyncSnapshot<T>.withData(ConnectionState.waiting, null);
+
+  @override
+  AsyncSnapshot<T> afterData(_, T data) => AsyncSnapshot<T>.withData(ConnectionState.active, data);
+
+  @override
+  AsyncSnapshot<T> afterError(_, Object error) => AsyncSnapshot<T>.withError(ConnectionState.active, error);
+
+  @override
+  AsyncSnapshot<T> afterDone(AsyncSnapshot<T> current) => current;
+
+  @override
+  AsyncSnapshot<T> afterDisconnected(AsyncSnapshot<T> current) => current;
+
+  @override
+  Widget build(BuildContext context, AsyncSnapshot<T> summary) {
+    if (summary.hasError) return _getError(context, summary.error);
+    if (summary.hasData) return builder(context, summary.data);
+    return _getLoading(context);
+  }
+
+  Widget _getLoading(BuildContext context) {
+    return loadingBuilder?.call(context) ?? Center(child: CircularProgressIndicator());
+  }
+
+  Widget _getError(BuildContext context, dynamic error) {
+    final _errorBuilder = errorBuilder ?? (context, dynamic error) {
+      error = error is Exception ? error.toString() : 'Error: $error';
+      return Center(child: Text(
+          error,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(backgroundColor: Colors.red, color: Colors.white),
+        )
+      );
+    };
+    return _errorBuilder.call(context, error);
+  }
+
+}
---
+++ test/flutter_data_stream_builder_test.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_data_stream_builder/flutter_data_stream_builder.dart';
+
+void main() {
+  
+  Widget _centeredText(String text) {
+    return Center(child: Text(text, textDirection: TextDirection.ltr));
+  }
+
+  Widget textBuilder(BuildContext context, dynamic data) {
+    return _centeredText(data.toString());
+  }
+
+  group('DataStreamBuilder', () {
+    testWidgets('correctly renders loading, error and data from stream', (tester) async {
+      final GlobalKey key = GlobalKey();
+      final controller = StreamController<int>();
+
+      await tester.pumpWidget(DataStreamBuilder<int>(
+        key: key, stream: controller.stream, builder: textBuilder,
+      ));
+
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+      controller.add(1);
+      controller.add(2);
+      await tester.pump(Duration.zero);
+      expect(find.text('2'), findsOneWidget);
+      
+      controller.add(3);
+      await tester.pump(Duration.zero);
+      expect(find.text('3'), findsOneWidget);
+
+      controller.addError(Exception('bad'));
+      await tester.pump(Duration.zero);
+      expect(find.text('Exception: bad'), findsOneWidget);
+
+      controller.addError('horrible');
+      await tester.pump(Duration.zero);
+      expect(find.text('Error: horrible'), findsOneWidget);
+
+      controller.add(4);
+      controller.close();
+      await tester.pump(Duration.zero);
+      expect(find.text('4'), findsOneWidget);
+    });
+
+    testWidgets('correctly renders custom loading and error widgets', (tester) async {
+      final GlobalKey key = GlobalKey();
+      final controller = StreamController<List<int>>();
+
+      await tester.pumpWidget(DataStreamBuilder<List<int>>(
+        key: key,
+        stream: controller.stream,
+        loadingBuilder: (context) => _centeredText('Loading numbers...'),
+        errorBuilder: (context, error) => _centeredText('Oops something went wrong!'),
+        builder: textBuilder
+      ));
+
+      expect(find.text('Loading numbers...'), findsOneWidget);
+
+      controller.add([1, 2]);
+      await tester.pump(Duration.zero);
+      expect(find.text('[1, 2]'), findsOneWidget);
+
+      controller.addError(Exception('bad'));
+      await tester.pump(Duration.zero);
+      expect(find.text('Oops something went wrong!'), findsOneWidget);
+
+      controller.close();
+    });
+  });
+
+}
